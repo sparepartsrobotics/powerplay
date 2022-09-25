29c906a8234a645b26fd4604d8f54d9d18062b3d
sparepartsrobotics <sparepartsrobotics@gmail.com>
2022-09-25 14:26:10 -0500

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode;
+
+ import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+ import com.qualcomm.robotcore.hardware.DcMotor;
+ import com.qualcomm.robotcore.hardware.DistanceSensor;
+ import com.qualcomm.robotcore.hardware.HardwareMap;
+ import com.qualcomm.robotcore.hardware.Servo;
+
+public class Robot {
+    //Drive Motors
+    public DcMotor frontRight;
+    public DcMotor frontLeft;
+    public DcMotor rearRight;
+    public DcMotor rearLeft;
+    public DcMotor duckSpinner;
+    public DcMotor intake;
+    public DcMotor slide;
+    public Servo stopArmServo;
+    public Servo clawArm;
+    public Servo claw;
+    public Servo clawTilt;
+    public DistanceSensor d1;
+    public DistanceSensor d2;
+
+    //    public DcMotor flyLeft;
+//    public DcMotor flyRight;
+//    public DcMotor beltMotor;
+//    public DcMotor wobblerMotor;
+//
+    public RevBlinkinLedDriver rgbDriver;
+//
+//    public Servo wobblerServo;
+//    public Servo stopServo;
+//    public Servo autoServo;
+//    public Servo autoLatchServo;
+//    public Servo rampServo;
+//    public Servo ringServo;
+//    public Servo wobblerArmServo;
+
+
+    public Robot(HardwareMap hardwareMap){
+
+
+        //Drive Motors
+        frontLeft = hardwareMap.dcMotor.get("lfWheel");
+        frontRight = hardwareMap.dcMotor.get("rfWheel");
+        rearLeft = hardwareMap.dcMotor.get("lrWheel");
+        rearRight = hardwareMap.dcMotor.get("rrWheel");
+        stopArmServo = hardwareMap.servo.get("stopArmServo");
+        duckSpinner = hardwareMap.dcMotor.get("dSpin");
+        intake = hardwareMap.dcMotor.get("intake");
+        slide = hardwareMap.dcMotor.get("slide");
+        clawArm = hardwareMap.servo.get("cA");
+        claw = hardwareMap.servo.get("claw");
+        clawTilt = hardwareMap.servo.get("cT");
+        d1 = hardwareMap.get(DistanceSensor.class, "d1");
+        d2 = hardwareMap.get(DistanceSensor.class, "d2");
+//        flyLeft = hardwareMap.dcMotor.get("flyLeft");
+//        flyRight = hardwareMap.dcMotor.get("flyRight");
+//        beltMotor = hardwareMap.dcMotor.get("beltMotor");
+//        wobblerMotor = hardwareMap.dcMotor.get("wobblerMotor");
+//        //Servos
+//        wobblerServo = hardwareMap.servo.get("wobblerServo");
+//        stopServo = hardwareMap.servo.get("StopServo");
+//        autoServo = hardwareMap.servo.get("autoServo");
+//        autoLatchServo = hardwareMap.servo.get("autoLatchServo");
+//        rampServo = hardwareMap.servo.get("rampServo");
+//        ringServo = hardwareMap.servo.get("ringServo");
+//        wobblerArmServo = hardwareMap.servo.get("wobblerArmServo");
+        rgbDriver = hardwareMap.get(RevBlinkinLedDriver.class,"ledDriver");
+
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        rearRight.setDirection(DcMotor.Direction.REVERSE);
+//        slide.setDirection(DcMotorSimple.Direction.REVERSE);
+//        flyRight.setDirection(DcMotor.Direction.REVERSE);
+//        beltMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+    }
+
+
+
+
+}
+
